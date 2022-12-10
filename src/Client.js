87d4c92d18cd6442cef5d26f4ee90ac76a4e89e2
子午 <wanglei@cai-inc.com>
2022-12-10 10:31:24 +0800

    add proxy

--- src/Client.js
+++ src/Client.js
@@ -1,9 +1,9 @@
 import { default as axios } from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://127.0.0.1:8000',
+    baseURL: 'http://localhost:3000/api',
     timeout: 1000,
-    headers: {'Authorization': 'Bearer d5219358-f96b-4bfc-8672-d01ba95d16fa'}
+    headers: {'Authorization': 'Bearer d5219358-f96b-4bfc-8672-d01ba95d16fa','X-Custom-Header': 'foobar'}
 })
 
 export default instance;
